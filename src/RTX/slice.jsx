f989f89d7b6915d25628d46f8f8980217f0dff3a
치훈 <rlaclgns321@naver.com>
2025-09-01 11:41:34 +0900

    feat/필요한 데이터 받기

---
+++ src/RTX/slice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchMultiplePokemonById } from './thunk';
+
+export const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState: {
+    data: [],
+    loading: true,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMultiplePokemonById.pending, (state) => {
+        state.loading = true;
+      }) // 처리중일때
+      .addCase(fetchMultiplePokemonById.rejected, (state) => {
+        state.loading = false;
+      }) // 잘 처리 못할때
+      .addCase(fetchMultiplePokemonById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      }); // 잘 처리됨
+  },
+});
